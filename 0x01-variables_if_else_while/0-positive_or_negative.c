4d838ffb16f42231605fc8f27e8cdeea1bddf6b6
Olayinka <theolayinkayodele@gmail.com>
2022-10-17 14:47:28 -0700

    generates random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,30 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there 
- * main - entry point
- * description : generates a randm number for n
- * return 0
- **/
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main fuction generate a random number
+ * Return: return 0(successful)
+ */
+
+
 int main(void)
+
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf("%d is zero\n", n);
 	}
-	else if (n < 0)
+	else
 	{
-		printf("is negative\n");
+		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
