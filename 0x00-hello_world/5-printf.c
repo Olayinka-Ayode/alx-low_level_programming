6faa6adb9af3110c3e810d79b7e44f7563de13c4
Olayinka <theolayinkayodele@gmail.com>
2022-10-17 12:21:36 -0700

    Prints a line using printf -C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - start point
+ * Description: C program that prints a line using printf
+ * Returns: 0
+*/
+int main (0) {
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
