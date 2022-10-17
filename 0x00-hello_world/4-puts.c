1f6bf757fac5cd11052979bb80407c870f01edeb
Olayinka <theolayinkayodele@gmail.com>
2022-10-17 12:15:17 -0700

    Prints a lin

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+
+/**
+ * main - Start point
+ * Description: C program that prints a sentence followed by a new line
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
