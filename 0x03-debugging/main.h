c6f7fd664310e228ad33deaf7313dc59805fc2c0
Olayinka <theolayinkayodele@gmail.com>
2022-10-20 14:16:30 +0100

    prints largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,18 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c)
+{
+    if (a >= b && a >= c) {
+        return a;
+    }
+    else if (b >= c && b >= a) {
+        return b;
+    }
+    else {
+        return c;
+    }
+}
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
